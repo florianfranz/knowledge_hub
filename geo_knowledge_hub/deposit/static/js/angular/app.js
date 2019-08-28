bee8a4e474d19222f1c8e64a14ea0c3e296d6477
raphaelrpl <raphael.wcosta@gmail.com>
2019-08-28 13:33:34 -0300

    Code review: directives directory

--- geo_knowledge_hub/deposit/static/js/angular/app.js
+++ geo_knowledge_hub/deposit/static/js/angular/app.js
@@ -1,10 +1,10 @@
 import angular from 'angular';
 
-import { dataSetDirective } from './dataset.directive';
-import { listItemDirective } from './list-item.directive';
-import { outputDirective } from './output.directive';
-import { publicationDirective } from './publication.directive';
-import { toolDirective } from './tool.directive';
+import { dataSetDirective } from './directives/dataset.directive';
+import { listItemDirective } from './directives/list-item.directive';
+import { outputDirective } from './directives/output.directive';
+import { publicationDirective } from './directives/publication.directive';
+import { toolDirective } from './directives/tool.directive';
 
 
 // Register Angular Application
