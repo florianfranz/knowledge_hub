dc154ec86253b3809cbca883444fd686e821d424
raphaelrpl <raphael.wcosta@gmail.com>
2019-08-30 08:42:36 -0300

    Fix component render

--- geo_knowledge_hub/deposit/static/js/angular/components/author.component.js
+++ geo_knowledge_hub/deposit/static/js/angular/components/author.component.js
@@ -1,25 +1,42 @@
 class AuthorController {
+  /** Initialize Author component variables */
   $onInit() {
     this.model = [];
     this.error = null;
   }
 
+  /** Add a new Author to the stack */
   addAuthor() {
+    if (!this.author)
+      return;
+
     this.model.push(this.author);
     this.author = "";
   }
 
+  /**
+   * Remove an author from cached list
+   *
+   * @param {number} index Author index to remove
+   */
   removeAuthor(index) {
-    console.log(this, index);
-    if (this.model.length >= index) {
-      return;
+    if (index >= 0 && index < this.model.length) {
+      this.model.splice(index, 1);
     }
-    this.model.splice(index, 1);
   }
 }
+// Angular injectors
 AuthorController.$inject = [];
 
-
+/**
+ * Defines an Brazil Data Cube Author component.
+ *
+ * The model consists in a list of string.
+ *
+ * @example
+ *
+ * <author id="myCustomId" model="$ctrl.authors"></author>
+ */
 export const authorComponent = {
   bindings: {
     id: '@',
@@ -30,12 +47,17 @@ export const authorComponent = {
     <div class="row">
       <div class="col-md-12">
         <div class="input-group">
-          <input type="text" class="form-control" placeholder="Author" ng-model="$ctrl.author">
+          <label for="{{ $ctrl.id }}">Authors: </label>
+
+          <input type="text" id="{{ $ctrl.id }}" ng-keydown="$event.keyCode === 32 && $ctrl.addAuthor()"
+                 class="form-control" placeholder="Author" ng-model="$ctrl.author">
 
           <span class="help-block" ng-if="!!$ctrl.error" ng-bind="$ctrl.error"></span>
 
           <span class="input-group-btn">
-            <button class="btn btn-default" type="button" ng-click="$ctrl.addAuthor()">Add!</button>
+            <button class="btn btn-success" style="margin-top: 24px" type="button" ng-click="$ctrl.addAuthor()">
+              <i class="fa fa-plus"></i>
+            </button>
           </span>
         </div><!-- /input-group -->
       </div><!-- /.col-md-12 -->
@@ -44,8 +66,6 @@ export const authorComponent = {
         <div class="author-list">
           <div class="form-group" ng-repeat="author in $ctrl.model track by $index">
             <div class="input-group">
-              <label for="{{ $ctrl.id }}-{{ $index + 1 }}">Authors: </label>
-
               <span class="input-group-addon">Author {{ $index+1 }}</span>
               <input type="text" class="form-control""
                       placeholder="" aria-describedby="basic-addon{{ $index + 1 }}"
